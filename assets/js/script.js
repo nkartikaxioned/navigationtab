b8713ec9bf9a372a6c81004d3f5ec5e0259362f6
nkartikaxioned <kartikn@axioned.com>
2023-10-03 14:52:36 +0530

    js add

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,31 @@
 
 */
 
-const image= document.querySelectorAll('.content');
+const images= document.querySelectorAll('.content');
+const lists = document.querySelectorAll('.nav-list li');
 
 
 
+lists.forEach((list,index) => {
 
+    list.addEventListener('click',() => {
+        hideImages();
+        hideList();
+        list.classList.add('active');
+        images[index].classList.add('show')
+    })
 
+})
+function hideImages(){
+    images.forEach(image => {
+        image.classList.remove('show');
+    })
+}
+function hideList(){
+    lists.forEach(list => {
+        list.classList.remove('active');
+    })
+}
 
 
 
